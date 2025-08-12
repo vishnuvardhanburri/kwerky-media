f6f8c834ffc193ad5275fdce094d67187bcac4d5
Ahmed Hanye <ahmedhanyehossny@gmail.com>
2025-08-12 23:19:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/awwwards/',
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': ['react', 'react-dom'],
-        },
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  server: {
-    port: 3000
-  }
 });
